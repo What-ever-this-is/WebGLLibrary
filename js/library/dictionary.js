ff9e704622f052c1d8cfa140f585fc778553304f
Che Yu <99137611+What-ever-this-is@users.noreply.github.com>
2022-12-14 17:24:00 -0600

    Development reset :(

---
+++ js/library/dictionary.js
@@ -0,0 +1,18 @@
+/**
+ * DICTIONARY FOR WEBGL
+ * bruh just abunch of keywords
+ */
+const glDictionary = {
+    VERTEX_SHADER:"vertexShader",
+    FRAGMENT_SHADER:"fragmentShader",
+    VERTEX_POSITION:"vertexPosition",
+    VERTEX_COLOR:"vertexColor",
+    VERTEX_UVCOORDS:"vertexUVCoords",//Will be implemented later
+    PROJECTION_MATRIX:"projectionMatrix",
+    VIEW_MATRIX:"viewMatrix",
+    TRANSLATION_MATRIX:"translationMatrix",
+    TEXTURED_VERTEX_SHADER:"usesTextureVertexShader",
+    COLORED_VERTEX_SHADER:"usesColorVertexShader",
+    TEXTURED_FRAGMENT_SHADER:"usesTextureFragmentShader",
+    COLORED_FRAgMENT_SHADER:"usesColorFragmentShader"
+}
